e023e07049c9147a5ecb4328748497e2e6afa0d7
Rafael Moraes <rafaelmoraes005@gmail.com>
2024-09-03 11:36:04 -0300

    [feat]: Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <nav class="navbar">
+      <div class="navbar-title">Morhinga</div>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Navbar',
+  }
+  </script>
+  
+  <style scoped>
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #3f51b5; 
+    color: white;
+    padding: 10px 20px;
+  }
+  
+  .navbar-title {
+    font-size: 24px;
+    font-weight: bold;
+  }
+  
+  </style>
+  
